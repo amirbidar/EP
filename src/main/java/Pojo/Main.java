e43ce239f436c59cd0c29fe00eafbed248b6db4f
AM.Bidar <amirbidar578@gmail.com>
2020-10-19 12:18:45 +0330

    Initial commit

---
+++ src/main/java/Pojo/Main.java
@@ -0,0 +1,10 @@
+package Pojo;
+
+public class Main {
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.setId(1L);
+        test.setAverage(12F);
+    }
+
+}
---
+++ src/main/java/Pojo/Test.java
@@ -0,0 +1,10 @@
+package Pojo;
+
+import lombok.Data;
+
+@Data
+public class Test {
+    public Long Id;
+    public Float Average;
+
+}
